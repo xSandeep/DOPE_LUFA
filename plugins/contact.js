6fcc375cc01505aa0093e32adfb82e7d046a31e6
Lufa <84374482+xSandeep@users.noreply.github.com>
2021-12-31 23:00:36 +0530

    DOPE_LUFA👽

--- plugins/contact.js
+++ plugins/contact.js
@@ -6,11 +6,11 @@ const fs = require('fs');
 const Config = require('../config')
 Ktb.addCommand({ pattern: 'owner ?(.*)', fromMe: false, desc: 'owner number' }, (async (message, match) => {
 
-//coded by saidali
+//coded by lufa
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Aju [OWNER]\n' // full name
-            + 'ORG:Aju;\n' // 
+            + 'FN:Lufa [OWNER]\n' // full name
+            + 'ORG:Lufa;\n' // 
             + 'TEL;type=CELL;type=VOICE;waid=917994873071:+91 7994873071\n' // WhatsApp ID + phone number
             + 'END:VCARD'
 await message.client.sendMessage(message.jid,{displayname: "Aju [OWNER]", vcard: vcard}, MessageType.contact)
